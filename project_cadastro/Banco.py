9c04d9818b18be3fbf63831ee25280326814c4ee
Nescafe006 <153241134+Nescafe006@users.noreply.github.com>
2024-09-12 21:41:54 -0300

    a

--- project_cadastro/Banco.py
+++ project_cadastro/Banco.py
@@ -1,19 +1,38 @@
 import sqlite3
 
+
 class Banco():
     def __init__(self):
         self.conexao = sqlite3.connect('banco.db')
+        self.conexao.execute("PRAGMA foreign_keys = ON")
         self.createTable()
 
     def createTable(self):
         c = self.conexao.cursor()
-        c.execute("""create table if not exists tbl_usuarios(
-        idusuario integer primary key autoincrement,
-        nome text,
-        telefone text,
-        email text,
-        usuario text,
-        senha text)""")
-        self.conexao.commit()
 
+        c.execute("""create table if not exists usuarios (
+                  id integer primary key autoincrement,
+                  nome text,
+                  telefone text,
+                  email text,
+                  usuario text,
+                  senha text
+                  )""")
+        c.execute("""create table if not exists cidades (
+                  id integer primary key autoincrement,
+                  cidade text,
+                  estado text
+                  )""")
+        c.execute("""create table if not exists clientes (
+                  id integer primary key autoincrement,
+                  nome text,
+                  telefone text,
+                  email text,
+                  endereco text,
+                    cpf text,
+                  cidade integer,
+                  FOREIGN KEY (id) REFERENCES cidades(id) ON DELETE RESTRICT
+                  )""")
+
+        self.conexao.commit()
         c.close()
