a0bfd02fdec7258cf63ed602245695d119886c5d
Nescafe006 <153241134+Nescafe006@users.noreply.github.com>
2024-09-12 21:40:36 -0300

    up

--- project_cadastro/usuarios.py
+++ project_cadastro/usuarios.py
@@ -1,68 +1,96 @@
 from Banco import Banco
-from tkinter import messagebox
-class Usuarios(object):
-    def __init__(self, idusuario = 0, nome = "", telefone = "",
-        email = "", usuario = "", senha = ""):
+
+
+class usuarios(object):
+
+    def __init__(self, id=0, nome="", telefone="", email="", usuario="", senha=""):
         self.info = {}
-        self.idusuario = idusuario
+        self.id = id
         self.nome = nome
         self.telefone = telefone
         self.email = email
         self.usuario = usuario
         self.senha = senha
 
-    def insertUser(self):
-        banco = Banco()
-        try:
-            c = banco.conexao.cursor()
-            c.execute("insert into tbl_usuarios (nome, telefone, email, usuario, senha) values ('" + self.nome + "', '" +
-            self.telefone + "', '" + self.email + "', '" +
-            self.usuario + "', '" + self.senha + "' )")
-            banco.conexao.commit()
-            c.close()
-            return "Usuário cadastrado com sucesso!"
-        except:
-            return "Ocorreu um erro na inserção do usuário"
+    def inserirUsuario(self):
+        b = Banco()
 
-    def updateUser(self):
-        banco = Banco()
         try:
-            c = banco.conexao.cursor()
-            c.execute("update tbl_usuarios set nome = '" + self.nome + "',telefone = '" + self.telefone + "', email = '" + self.email +
-            "', usuario = '" + self.usuario + "', senha = '" + self.senha +
-            "' where idusuario = " + self.idusuario + " ")
-            banco.conexao.commit()
-            c.close()
-            return "Usuário atualizado com sucesso!"
-        except:
-            return "Ocorreu um erro na alteração do usuário"
+            c = b.conexao.cursor()
 
-    def deleteUser(self):
-        banco = Banco()
-        try:
-            c = banco.conexao.cursor()
-            c.execute("delete from tbl_usuarios where idusuario = " + self.idusuario + " ")
-            banco.conexao.commit()
-            c.close()
-            return "Usuário excluído com sucesso!"
-        except:
-            return "Ocorreu um erro na exclusão do usuário"
+            c.execute(
+                "insert into usuarios(nome, telefone, email, usuario, senha) values ('" + self.nome + "','" + self.telefone + "','" + self.email + "','" + self.usuario + "','" + self.senha + "')")
 
-    def selectUser(self, idusuario):
-        banco = Banco()
+            b.conexao.commit()
+            c.close()
+            return "Usuario cadastrado com sucesso!"
+        except:
+            return "Ocorreu um erro!"
+
+    def alterarUsuario(self):
+        b = Banco()
         try:
-            c = banco.conexao.cursor()
-            c.execute("select * from tbl_usuarios where idusuario = " + idusuario + " ")
+            c = b.conexao.cursor()
+
+            c.execute(
+                "update usuarios set nome = '" + self.nome + "', telefone = '" + self.telefone + "', email = '" + self.email + "', usuario = '" + self.usuario + "', senha = '" + self.senha + "' where id = " + self.id + " ")
+
+            b.conexao.commit()
+            c.close()
+
+            return "Usuario atualizado com sucesso!"
+        except:
+            return "Ocorreu um erro na alteração do usuario!"
+
+    def excluirUsuario(self):
+        b = Banco()
+        try:
+
+            c = b.conexao.cursor()
+
+            c.execute("delete from usuarios where id = " + self.id + " ")
+
+            b.conexao.commit()
+
+            c.close()
+
+            return "Usuario excluido com sucesso"
+        except:
+            return "Ocorreu um erro na exclusão do usuario"
+
+    def buscarUsuario(self):
+        b = Banco()
+
+        try:
+            c = b.conexao.cursor()
+
+            c.execute("select * from usuarios where id = " + self.id + " ")
+
             for linha in c:
-                self.idusuario = linha[0]
+                self.id = linha[0]
                 self.nome = linha[1]
                 self.telefone = linha[2]
                 self.email = linha[3]
                 self.usuario = linha[4]
                 self.senha = linha[5]
+
             c.close()
-            return messagebox.showinfo("", "busca feita com sucesso")
 
-
+            return "Busca feita com sucesso"
         except:
-            return messagebox.showerror("", "erro na busca")
+            return "Ocorreu um erro na busca"
+
+    def buscarTreeView(self):
+        b = Banco()
+        c = b.conexao.cursor()
+
+        c.execute("select * from usuarios")
+
+        rows = []
+
+        for linha in c:
+            rows.append((linha[0], linha[1], linha[2], linha[3], linha[4], linha[5]))
+
+        c.close()
+
+        return rows
